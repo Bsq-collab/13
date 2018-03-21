416b549b1f6a06a666f89ac07e96e3434c9e9cef
Bayan Berri <bberri@stuy.edu>
2018-03-21 16:14:05 -0400

    comments

--- script.js
+++ script.js
@@ -14,7 +14,10 @@ var height=svg.getAttribute("height");
 //X= Temperature, Y=Robberies
 data=[[12,33.18],[15,31.54],[17,33.95],[19,37],[21,35.16],[24,37],[26,35.84],[28,39.31],[30,39.20],[33,38.44],[35,38.70],[37,40.53],[39,40],[42,40.27],[44,41.80],[46,42.45],[48,43.71],[51,45.46],[53,44]]
 
-var setupAxes = function() {
+//====================================================================
+//                      SETS UP THE AXES OF THE GRAPH
+//====================================================================
+var setup = function() {
   var labels = d3.select("svg").selectAll("text").data(data).enter();
   labels.append("text")
     .attr("x", function(d) { return 10 * d[0] + 5; })//spreads them out
@@ -30,15 +33,18 @@ var setupAxes = function() {
       }});
 };
 
-var plot = function() {
+//===================================================================
+//                     Scales and plots the points
+//===================================================================
+var plot_points = function() {
   var circles = d3.select("svg").selectAll("circle").data(data).enter();
   circles.append("circle")
     .attr("cx", function(d) { return 9 * d[0] + 20; })
     .attr("cy", function(d) { return 500 - d[1] * 8; })
-    .attr("r", 5)
+    .attr("r", 6)
     .attr("fill", "black");
   svg.append(circles);
 };
 
-setupAxes();
-plot();
+setup();
+plot_points();
